920aa93edfb7f0c5ece239743728922da7f866aa
Sean <crimsonsand@live.com>
2022-05-07 19:43:41 -0700

    Add successful test 2 of back end

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
-function Pizza(size) {
+function Pizza(size, topping) {
   this.size = size;
+  this.topping = topping;
 }
 
-let pizza = new Pizza("foal");
+let pizza = new Pizza("foal","cotton candy");
 console.log(pizza.size);
+console.log(pizza.topping);
+
