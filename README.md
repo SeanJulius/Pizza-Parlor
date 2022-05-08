5a6ec4e8b203b1c2295c542e54943215d1fc4e3b
Sean <crimsonsand@live.com>
2022-05-08 00:27:34 -0700

    Add Html and some Css for style

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
+# Pizza-Parlor
+
+by Sean Julius
+
+This webpage is designed to give an introduction to Sean Julius's beginnings in coding.
+Technologies Used
+* _Java Script_
+* _HTML_
+* _CSS_
+
+## Application Setup Instructions
+
+Clone this repository to your desktop.
+Navigate to the top level of the directory.
+Open Portfolio-Landing-Page
+Open Index.html
+
+## Known Bugs
+
+No known bugs
+
+
+Copyright (c) 2022 Sean Julius
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+## Specifications
+
 Describe: Pizza()
 
 Test: "it should return pizza size."
--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,46 @@
-//Business Logic
+// Business logic
 
-function Pizza(size, topping) {
+function Pizza (size, topping){
   this.size = size;
   this.topping = topping;
-}
-
-Pizza.prototype.calculator = function() {
-  let cost = 0;
-  if (this.size === "foal") {
-    cost = 10;
   }
-  else if (this.size === "mare") {
-    cost = 15;
-  }
-  else if (this.size === "stallion") {
-    cost = 20;
-  }
-  this.topping.forEach(function() {
-    cost++;
-  });
-  return cost;
-}
-
-// let pizza = new Pizza("foal",["cotton candy","sprinkles","rainbow sherbet"]);
-// let pizzaCost = pizza.calculator();
-// console.log(pizza.size);
-// console.log(pizza.topping);
-// console.log(pizzaCost);
-
-//User Logic
-
-$(document).ready(function() {
-  $("#menu").submit(function(event) {
+  
+  Pizza.prototype.calculate = function(){
+    let total = 0;
+    if (this.size === "foal") {
+      total += 10;
+    } else if (this.size === "mare") {
+      total += 15;
+    } else if (this.size === "stallion") {
+      total += 20;
+    }
+    if (this.topping === "cotton candy") {
+      total = total + 1;
+    } else if (this.topping === "sprinkles") {
+      total = total + 2;
+    } else if (this.topping === "rainbow sherbet") {
+      total = total + 8;
+    }
+    return total;
+  };
+  
+//   // UI logic
+  $(document).ready(function(){
+    $(".pizzaOption").submit(function(event){
     event.preventDefault();
-    let pizzaSize = $("#pizzaSize").val();
-    let arrayOfToppings = document.querySelectorAll("input[name = 'topping']:checked");
-    let arrayTopping = [];
-    arrayOfToppings.forEach(function(toppingCheck) {
-      arrayTopping.push(toppingCheck.value);
-    });
-    let menuPizza = new Pizza(pizzaSize,arrayTopping);
-    let menuCost = menuPizza.calculator();
-    console.log(menuCost);
-    $("#menuShown").toggle();
-    $("#costReturn").toggle();
-    $("#putCostHere").html("$" + menuCost);
-  });
-});
+  
+    let inputtedSize = $("#size").val();
+    let inputtedTopping = $("#topping").val();
+  
+    let newPizza = new Pizza(inputtedSize, inputtedTopping);
+    let total = newPizza.calculate();
+  
+    $(".results").show();
+    $(".totalPrice").show();
 
+    $(".temporary").hide();
+    $("#sizeResult").text("Size: " + inputtedSize);
+    $("#toppingResult").text("Topping: " + inputtedTopping);
+    $("#total").text("Total: " + total);
+    });
+  });
